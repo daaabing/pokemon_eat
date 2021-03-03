2fbe1f25561693d9ae6842f045cfe410799676bc
Chuan Zheng <zhengchuan000@gmail.com>
2021-03-03 10:42:42 -0500

    login and signup error detection and DB seed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,53 +5,120 @@ require "optparse"
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  @@API_KEY = "cOvuE6J1hqqbEn_ByZMHUqXod80KtPv55iLvQ6G9zoiFtbYvLNtpzUrHE9cFnP4-jTl-5ha99bTNYQ7LAtCWJ2FerNpFlRF9KVBgFmNyXQ9YxPBkNj_DLElVq1M-YHYx"
+  @@API_HOST = "https://api.yelp.com"
+  @@SEARCH_PATH = "/v3/businesses/search"
+  @@BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+  @@DEFAULT_BUSINESS_ID = "yelp-san-francisco"
 
-
-
-
+  #GET /search
   def search
-    @API_KEY = "YzXFUy2VurAbmBHqBTiZwRC9Mx5rTGHkaZs5Jv-_V2K4BpLCtv0ucQOlT7slIGGHU2I8r-5kUDgJ0c6k5I1BYtPHWvCAV0n7DetPpKrxHhYcq6Dcku8FdJjA9o87YHYx"
-    @API_HOST = "https://api.yelp.com"
-    @SEARCH_PATH = "/v3/businesses/search"
-    @BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+    if params.has_key?("commit") == true
+      render "search"
+    end
 
+    @TERM = "dinner"
+    if params[:term] != nil
+      @TERM = params[:term]
+    end
 
-    @DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-    @DEFAULT_TERM = "dinner"
-    @DEFAULT_LOCATION = "San Francisco, CA"
-    @SEARCH_LIMIT = 5
-    url = "#{@API_HOST}#{@SEARCH_PATH}"
-    params = {
-      term: "seafood",
-      location: "New York",
-      limit: @SEARCH_LIMIT
+    @LOCATION = "San Francisco, CA"
+    if params[:location] != nil
+      @LOCATION = params[:location]
+    end
+
+    @url = "#{@@API_HOST}#{@@SEARCH_PATH}"
+    @params = {
+      term: @TERM,
+      location: @LOCATION,
+      limit: 5
     }
-    response = HTTP.auth("Bearer #{@API_KEY}").get(url, params: params)
-    puts response
+    @response = HTTP.auth("Bearer #{@@API_KEY}").get(@url, params: @params)
+    @response_body_hash = JSON.parse(@response.body)
+    @businesses = @response_body_hash["businesses"]
+    puts "------------"
+    puts "is hash"
+    puts @businesses[0].is_a? Hash
+    puts "------------"
+    render "search"
   end
 
 
 
 
   def login
-    @email = params[:address]
+    @email = params[:email]
     @password = params[:password]
-    if User.find_by(email: @email, password_digest: @password) != nil 
-      redirect_to "http://www.google.com"
+    @user = User.find_by(email: @email)
+    flash[:login_error] = []
+    if @email == ""
+      flash[:login_error].push("Empyt email!")
+    end
+    if @password == ""
+      flash[:login_error].push("Empty password!")
+    elsif @password != @user.password_digest
+      flash[:login_error].push("Invalid password!")
+    end
+
+    if flash[:login_error].length() > 0
+      puts flash[:login_error]
+      render"pages/welcome"
+    else
+      redirect_to action: "show", id:@user.id
     end
   end
 
+
+
   def signup
-    @email = params[:address]
+    @email = params[:email]
     @password = params[:password]
-    if User.find_by(email: @email) != nil #buggy!!!
-      flash[:registered] = "This email has been registered."
+    @re_password = params[:re_password]
+    flash[:signup_error] = []
+    if @email == ""
+      flash[:signup_error].push("Empyt email!")
+      # puts "Empyt email!"
+    end
+    if @password == "" or @re_password == ""
+      flash[:signup_error].push("Empty password or re-password!")
+      # puts "Empty password or re-password!"
+    end
+    if @password != @re_password
+      flash[:signup_error].push("Password and Re-password do not match!")
+      # puts "Password and Re-password do not match!"
+    end
+    if User.find_by(email: @email) != nil
+      flash[:signup_error].push("This email has been registered!")
+      # puts "This email has been registered!"
+    end
+
+    if flash[:signup_error].length() > 0
+      puts flash[:signup_error]
+      render"pages/welcome"
     else
       @new_user = User.create(email: @email, password_digest: @password)
       @new_user.save()
-      redirect_to "http://www.google.com"
+      flash[:signup_error] = nil
+      redirect_to action: "show", id:@new_user.id
     end
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   # GET /users or /users.json
   def index
@@ -59,7 +126,7 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1 or /users/1.json
-  def show
+  def show      
   end
 
   # GET /users/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([
+    {email: "zhengchuan000@gmail.com", password_digest: "1"},
+    {email: "807442894@qq.com", password_digest: "1"},
+    {email: "hzwang@ucdavis.edu", password_digest: "1"},
+    {email: "heyunong1223@gmail.com", password_digest: "1"},
+])
