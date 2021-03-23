697a48ed7ecc3aa60695ceb28a9086b6a2e90ee1
daaabing <46662317+daaabing@users.noreply.github.com>
2021-03-23 00:52:06 -0700

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_024232) do
+ActiveRecord::Schema.define(version: 2021_03_20_155950) do
+
+  create_table "options", force: :cascade do |t|
+    t.text "option"
+    t.string "choice"
+    t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_options_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "question"
+    t.text "answer"
+    t.string "question_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "email"
