bb2eb00f77291645217889a03d256ecab56a71ca
Chuan Zheng <zhengchuan000@gmail.com>
2021-03-28 14:25:39 -0400

    ambiguous match bug

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -10,15 +10,17 @@ When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
   fill_in(field, :with => value)
 end
 
-
 When /^(?:|I )follow "([^"]*)"$/ do |link|
   click_link(link)
+end
+
+When /^(?:|I )press "(Around)"$/ do |button|
+  click_button(button)
 end
 
 When /^(?:|I )press "(.*)"$/ do |button|
   click_button(button)
 end
-
 
 Then /^(?:|I )should see "(.*)"$/ do |text|
   if page.respond_to? :should
