c30bcde8b05045032eb97b5d7c7a504c523d87fc
Chuan Zheng <zhengchuan000@gmail.com>
2021-04-12 19:51:17 -0400

    add precompile config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
+  
+  config.assets.compile = true
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -95,11 +98,13 @@ Rails.application.configure do
 
   config.active_record.cache_versioning = false
 
+  config.assets.digest = false
 
 
 
 
-  
+
+
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
